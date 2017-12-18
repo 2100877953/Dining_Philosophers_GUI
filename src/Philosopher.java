09bca8c4fe28296f37e5c9138b15e409d6e6dd57
Ege Alpay <egealpay1@gmail.com>
2017-12-18 22:16:50 +0300

    Files added

---
+++ src/Philosopher.java
@@ -0,0 +1,458 @@
+import java.awt.EventQueue;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+class Table {
+
+	// FOR GUI:
+	static JFrame frame; 
+	private JLabel[] plates = new JLabel[5];
+	private JLabel[] forks = new JLabel[5];
+	static Table window;
+	static BufferedImage fork;
+
+	/**
+	 * Launch the application.
+
+	/**
+	 * Create the application.
+	 */
+	public Table() {
+
+
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.WHITE);
+		BufferedImage plate = null;
+		try {
+			plate = ImageIO.read(new File("spaghetti_blue.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		frame.getContentPane().setLayout(null);
+		plates[0] = new JLabel(new ImageIcon(plate));
+		plates[0].setBounds(185, 10, 100, 100);
+		frame.getContentPane().add(plates[0]);
+		plates[0].setVisible(false);
+
+		plates[1] = new JLabel(new ImageIcon(plate));
+		plates[1].setBounds(300, 100, 100, 100);
+		frame.getContentPane().add(plates[1]);
+		plates[1].setVisible(false);
+
+		plates[2] = new JLabel(new ImageIcon(plate));
+		plates[2].setBounds(230, 230, 100, 100);
+		frame.getContentPane().add(plates[2]);
+		plates[2].setVisible(false);
+
+		plates[3] = new JLabel(new ImageIcon(plate));
+		plates[3].setBounds(70, 210, 100, 100);
+		frame.getContentPane().add(plates[3]);
+		plates[3].setVisible(false);
+
+		plates[4] = new JLabel(new ImageIcon(plate));
+		plates[4].setBounds(50, 80, 100, 100);
+		frame.getContentPane().add(plates[4]);
+		plates[4].setVisible(false);
+
+		try {
+			fork = ImageIO.read(new File("fork_white_1.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		forks[0] = new JLabel(new ImageIcon(fork));
+		forks[0].setBounds(250, 40, 100, 100);
+		frame.getContentPane().add(forks[0]);
+
+		try {
+			fork = ImageIO.read(new File("fork_white_2.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		forks[1] = new JLabel(new ImageIcon(fork));
+		forks[1].setBounds(280, 170, 100, 100);
+		frame.getContentPane().add(forks[1]);
+
+		try {
+			fork = ImageIO.read(new File("fork_white_3.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		forks[2] = new JLabel(new ImageIcon(fork));
+		forks[2].setBounds(150, 220, 100, 100);
+		frame.getContentPane().add(forks[2]);
+
+		try {
+			fork = ImageIO.read(new File("fork_white_4.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		forks[3] = new JLabel(new ImageIcon(fork));
+		forks[3].setBounds(60, 145, 100, 100);
+		frame.getContentPane().add(forks[3]);
+
+		try {
+			fork = ImageIO.read(new File("fork_white_5.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		forks[4] = new JLabel(new ImageIcon(fork));
+		forks[4].setBounds(95, 15, 100, 100);
+		frame.getContentPane().add(forks[4]);
+	}
+
+
+	public void PutPlate_GUI(int i)
+	{
+		plates[i].setVisible(true);
+	}
+
+	public void StartDining_GUI(int i)
+	{
+		try {
+			plates[i].setIcon(new ImageIcon(ImageIO.read(new File("spaghetti_white.jpg"))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void Hungry_GUI(int i) {
+
+		try {
+			plates[i].setIcon(new ImageIcon(ImageIO.read(new File("spaghetti_yellow.jpg"))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void ForkTake_GUI(int i) {
+		try{
+			if (i == 0)
+			{
+				forks[0].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_1.jpg"))));
+				forks[4].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_5.jpg"))));
+			}
+			else if (i == 1)
+			{
+				forks[0].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_1.jpg"))));
+				forks[1].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_2.jpg"))));
+			}
+			else if (i == 2)
+			{
+				forks[1].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_2.jpg"))));
+				forks[2].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_3.jpg"))));
+			}
+			else if (i == 3)
+			{
+				forks[2].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_3.jpg"))));
+				forks[3].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_4.jpg"))));
+			}
+			else if (i == 4)
+			{
+				forks[3].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_4.jpg"))));
+				forks[4].setIcon(new ImageIcon(ImageIO.read(new File("fork_red_5.jpg"))));
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void Eating_GUI(int i) {
+		try {
+			plates[i].setIcon(new ImageIcon(ImageIO.read(new File("spaghetti_red.jpg"))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex){
+			ex.printStackTrace();
+		}
+	}
+
+	public void ForkPut_GUI(int i) {
+		try{
+			if (i == 0)
+			{
+				forks[0].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_1.jpg"))));
+				forks[4].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_5.jpg"))));
+			}
+			else if (i == 1)
+			{
+				forks[0].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_1.jpg"))));
+				forks[1].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_2.jpg"))));
+			}
+			else if (i == 2)
+			{
+				forks[1].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_2.jpg"))));
+				forks[2].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_3.jpg"))));
+			}
+			else if (i == 3)
+			{
+				forks[2].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_3.jpg"))));
+				forks[3].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_4.jpg"))));
+			}
+			else if (i == 4)
+			{
+				forks[3].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_4.jpg"))));
+				forks[4].setIcon(new ImageIcon(ImageIO.read(new File("fork_white_5.jpg"))));
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void StopEating_GUI(int i) {
+		try {
+			plates[i].setIcon(new ImageIcon(ImageIO.read(new File("spaghetti_white.jpg"))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
+
+public class Philosopher extends Thread {
+
+	private static Table table;
+
+	private int THINKING = 0;
+	private int HUNGRY = 1;
+	private int EATING = 2;
+
+
+	private Semaphore [] sem;
+	private Semaphore[] barriers;
+	private Semaphore mutex;
+	private int id;
+
+
+	private int [] state;
+	private int N;
+
+	public Philosopher(int i, Semaphore [] s, int [] mystate, int philnumber, Semaphore mu, Semaphore[] bar)
+	{
+		id = i;
+		sem = s;
+		state = mystate;
+		N = philnumber;
+		mutex = mu;
+		barriers = bar;
+	}
+
+	
+	public static void main(String args[]) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					table = new Table();
+					table.frame.setVisible(true);
+
+				}
+				catch(Exception e){
+					e.printStackTrace();
+
+				}
+			}
+		});
+
+
+		int N = 5;
+		int [] state = new int [N];
+
+
+		Semaphore mutex = new Semaphore (1);
+		Semaphore [] semarray = new Semaphore [N];
+		Semaphore [] bararray = new Semaphore [N];
+
+		Philosopher[] oldies = new Philosopher [N];
+
+		for (int i=0; i < N; i++)
+		{
+			semarray[i] = new Semaphore(0,true);	
+			bararray[i] = new Semaphore(0,true);
+		}
+		for (int i=0; i < N; i++)
+		{
+			oldies[i] = new Philosopher(i, semarray, state, N, mutex,bararray);
+			oldies[i].start();
+			try {
+				sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub		
+		Random rand = new Random();
+		int num = rand.nextInt(10) + 1; //min is 1, max is 10
+		
+		try {
+			sleep(num * 1000); //Think as walking to table(in msec)
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		table.PutPlate_GUI(id); //Philosopher has arrived to table, put his plate to table
+		
+		try //Barrier. Philosopher is waiting for other philosophers
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				barriers[id].release(); //up on its own barrier 4 times
+			}
+			for(int j = 0; j < 5; j++)
+			{
+				if(id != j)
+					barriers[j].acquire(); //donw on other barriers
+			}
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		
+		table.StartDining_GUI(id); //All philophers are ready, then start dining
+		
+		while(true) //Loop forever
+		{
+			int thinkingTime = rand.nextInt(11) ; //min is 0, max is 11
+						
+			try {
+				sleep(thinkingTime * 1000); //Philosopher is thinking(msec)
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			take_forks(id); //Try to take forks
+		
+			eat(id); //Philosopher is eating
+			
+			put_forks(id); //Put forks back to table
+		}
+	}
+	
+	public void take_forks(int id)
+	{
+		try {
+			mutex.acquire(); //Down on mutex
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		state[id] = HUNGRY; //set state is Hungry
+		table.Hungry_GUI(id);
+		
+		try {
+			sleep(500); //Sleep for 0.5 seconds
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		test(id); //Check if left and right is eating or not
+		
+		mutex.release(); //up on mutex
+		
+		try {
+			sem[id].acquire(); //down on its semaphore
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void test(int id)
+	{
+		int LEFT = (id + 5 - 1) % 5;
+		int RIGHT = (id + 1) % 5;
+		
+		if(state[id] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) //Check if left and right is eating
+		{
+			state[id] = EATING; //They are not eating, so I can eat
+			table.ForkTake_GUI(id); //Take forks
+			sem[id].release(); //Up on its Semaphore
+		}
+	}
+	
+	public void eat(int id)
+	{
+		table.Eating_GUI(id);
+		
+		try {
+			sleep(3000); //Sleep for 3 seconds(eating for 3 seconds)
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	public void put_forks(int id)
+	{
+		try {
+			mutex.acquire(); //Down on mutex
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		table.StopEating_GUI(id);
+		
+		state[id] = THINKING; //Now, it is going to think
+		
+		int LEFT = (id + 5 - 1) % 5;
+		int RIGHT = (id + 1) % 5;
+		
+		table.ForkPut_GUI(id); //Put Forks back to table
+		
+		test(LEFT); //Check left
+		test(RIGHT); //check right
+		
+		
+		mutex.release(); //up on mutex
+	}
+}
+
